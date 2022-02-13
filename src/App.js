58d5f73729d3216e7884a6cb0167f8fc557184a3
Ardeshir <ardeshiir@outlook.com>
2022-02-13 12:20:45 +0330

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AnimatePresence } from "framer-motion";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+//Pages
+import Home from "./pages/Home";
+import Model from "./pages/Model";
+//components
+import Header from "./components/Header";
+//Styles
+import "./App.scss";
 
 function App() {
+  const location = useLocation();
+
+  const imageDetails = {
+    width: 524,
+    height: 650,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <AnimatePresence initial={false} exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home imageDetails={imageDetails} />} />
+          <Route
+            path="/model/:id"
+            element={<Model imageDetails={imageDetails} />}
+          />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+const Home = ({ imageDetails, image }) => (
+  <>
+    <main>
+      <div className="container">
+        <div className="row center">
+          <div className="image-container">
+            <div
+              className="thumbnail"
+              ref={image}
+              style={{
+                width: imageDetails.width,
+                height: imageDetails.height,
+              }}
+            >
+              <div className="frame">
+                <Link to={`/model/yasmeen-tariq`}>
+                  <motion.img
+                    alt="Yasmeen Tariq"
+                    whileHover={{ scale: 1.1 }}
+                    transition={transition}
+                    src={"/model.jpg"}
+                  />
+                </Link>
+              </div>
+            </div>
+            <motion.div
+              exit={{ opacity: 0 }}
+              transition={transition}
+              className="information"
+            >
+              <div className="title">Cute Kitty</div>
+              <div className="location">
+                <span>28.538336</span>
+                <span>-81.379234</span>
+              </div>
+            </motion.div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </>
+);
+
+export default Home;
